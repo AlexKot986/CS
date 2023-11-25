1b79a60e80d750ff6a257842aba8a6410e7f659e
Alexershov86 <alexershov86@mail.ru>
2023-11-25 21:02:59 +0300

    homework cs 1

---
+++ SeminarCS1/FamilyMambers.cs
@@ -0,0 +1,35 @@
+﻿
+namespace SeminarCS1
+{
+    internal class FamilyMambers
+    {
+        private FamilyMambers wife;
+        private FamilyMambers husband;
+        private FamilyMambers[] children;
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public Gender Gender { get; set; }
+        public FamilyMambers Mother { get; set; }
+        public FamilyMambers Father { get; set; }      
+        public FamilyMambers Wife 
+        { 
+            get { return wife; } 
+            set { if (Gender is Gender.man) wife = value; }
+        }       
+        public FamilyMambers Husband 
+        { 
+            get { return husband; } 
+            set { if (Gender is Gender.woman) husband = value; } 
+        }       
+        public FamilyMambers[] Children 
+        {
+            get { return children; }
+            set { AddChildren(value); } 
+        }
+
+        public FamilyMambers[] GetGrandMothers() => new FamilyMambers[] { Mother?.Mother, Father?.Mother };     
+        public FamilyMambers[] GetGrandFathers() => new FamilyMambers[] { Mother?.Father, Father?.Father };
+        public FamilyMambers GetCloseRelative() => wife ?? husband;
+        private void AddChildren(params FamilyMambers[] children) => this.children = children;
+    }
+}
---
+++ SeminarCS1/PrintPerson.cs
@@ -0,0 +1,53 @@
+﻿
+using System.Text;
+
+namespace SeminarCS1
+{
+    internal static class PrintPerson
+    {
+        public static void PrintGrandMothers(this FamilyMambers person)
+        {
+            int counter = 0;
+            StringBuilder sb = new();
+            sb.Append("---------------------------------------------\n");
+            sb.Append(person.FullName + " все бабушки:\n");
+            foreach (FamilyMambers fm in person.GetGrandMothers())
+                if (fm is not null)
+                    sb.Append(string.Format("{0}. {1}\n", ++counter, fm.FullName));
+            sb.Append("---------------------------------------------");
+            Console.WriteLine(sb.ToString());
+        }
+        public static void PrintGrandFathers(this FamilyMambers person)
+        {
+            int counter = 0;
+            StringBuilder sb = new();
+            sb.Append("---------------------------------------------\n");
+            sb.Append(person.FullName + " все дедушки:\n");
+            foreach (FamilyMambers fm in person.GetGrandFathers())
+                if (fm is not null) 
+                    sb.Append(string.Format("{0}. {1}\n", ++counter, fm.FullName));
+            sb.Append("---------------------------------------------");
+            Console.WriteLine(sb.ToString());
+        }
+        public static void PrintCloseRelative(this FamilyMambers person)
+        { 
+            StringBuilder sb = new();
+            sb.Append("---------------------------------------------\n");
+            sb.Append(person.FullName + " супруг:\n");
+            sb.Append(string.Format("-  {0}\n", person.GetCloseRelative().FullName));
+            sb.Append("---------------------------------------------");
+            Console.WriteLine(sb.ToString());
+        }
+        public static void PrintChildren(this FamilyMambers person)
+        {
+            int counter = 0;
+            StringBuilder sb = new();
+            sb.Append("---------------------------------------------\n");
+            sb.Append(person.FullName + " дети:\n");
+            foreach (FamilyMambers fm in person.Children)
+                sb.Append(string.Format("{0}. {1}\n", ++counter, fm.FullName));
+            sb.Append("---------------------------------------------");
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
